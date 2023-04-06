b364b596d3bc74e6e9e5ddf823274df91a112d94
ranggabotwolf <kecul37@gmail.com>
2023-04-06 21:00:54 +0700

    Accepting Arguments

---
+++ belajar/accepting_arguments.py
@@ -0,0 +1,10 @@
+# Accepting Arguments
+import sys
+
+print(sys.argv)
+
+# Berikut adalah contoh Accepting Arguments
+# Jadi ketika kita menjalankan file ini kedalam shell dengan menuliskan seperti ini :
+# python [lokasi_file/nama_file] yang ingin kita tampilkan, contoh Rangga
+# Maka program yang akan tampil adalah ['lokasi_file/nama_file', 'Rangga']
+# Semua berada dalam string
