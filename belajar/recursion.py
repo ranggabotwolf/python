5b859f646fb207e3a748d213d02949c39140496c
ranggabotwolf <kecul37@gmail.com>
2023-04-07 11:53:05 +0700

    recursion

---
+++ belajar/recursion.py
@@ -0,0 +1,21 @@
+# Recursion
+# function dalam python dapat memanggil dirinya sendiri, yang demikian disebut recursion
+
+# Contoh faktorial
+# 3! = 3 * 2 * 1 = 6
+# 2! = 2 * 1 = 2
+
+
+# Menggunakan recursion, kita bisa menuliskan function yang menghitung faktorial
+def factorial(n):
+    if n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+
+# diatas adalah function dalam function memanggil function yang sama
+# recursive function selalu mempunyai base case (baris ke 11-12), dan recursive case (baris ke 13)
+# base case adalah ketika program keluar dari recursive function
+# Kita harus membuat base case jadi functionnya bisa berhenti
+# bila kita tidak membuat base case nya, maka yang akan terjadi adalah infinite / error
+print(factorial(3))
