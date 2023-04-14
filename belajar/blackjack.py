7ccfb487c8bfee3ef02c4c736b78dafc5fb8ca64
ranggabotwolf <kecul37@gmail.com>
2023-04-14 19:46:18 +0700

    blackjack 14 April

--- belajar/blackjack.py
+++ belajar/blackjack.py
@@ -1,3 +1,4 @@
+import random
 import itertools
 
 cards = []
@@ -7,5 +8,35 @@ ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 value = 10
 
 print(f"Your card is: {ranks} of {suit}")
+print("\n")
+# April, 14 2023
 for suit, rank in itertools.product(suits, ranks):
-    print([suit, rank])
+    cards.append([suit, rank])
+
+
+def shuffle():
+    random.shuffle(cards)
+
+
+def deal(number):
+    card_dealt = []
+    for x in range(number):
+        card = cards.pop()
+        card_dealt.append(card)
+    return card_dealt
+
+
+shuffle()
+cards_dealt = deal(2)
+card = cards_dealt[0]
+rank = card[1]
+
+if rank == "A":
+    value = 11
+elif rank in ["J", "K", "Q"]:
+    value = 10
+else:
+    value = rank
+
+rank_dict = {"rank": rank, "value": value}
+print(rank_dict["rank"], rank_dict["value"])
