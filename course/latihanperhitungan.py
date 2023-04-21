12af8747a07bfc66984f09d52fd5784cdbacac17
ranggabotwolf <kecul37.com>
2023-04-21 16:14:04 +0700

    course indonesia

---
+++ course/latihanperhitungan.py
@@ -0,0 +1,9 @@
+# Program konversi temperatur
+
+print("PROGRAM KONVERSI TEMPERATUR\n")
+
+celcius = float(input("Masukkan suhu dalam celcius :"))
+print("Suhu dalam celsius :", celcius, "celcius")
+
+
+reamur = (4 / 5) * celcius
