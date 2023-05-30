c3cab25a635663f6faadf257f8ce27f9d50228b0
ranggabotwolf <kecul37.com>
2023-05-30 11:28:46 +0700

    latihan elif

---
+++ course/latihan_elif.py
@@ -0,0 +1,11 @@
+# ELIF Statement
+nama = input("Siapa nama kamu? ")
+
+if nama == "Rangga":  # kondisi 1
+    print("ini benar kamu Rangga")
+elif nama == "Putra":  # kondisi 2
+    print("oh kamu Putra")
+elif nama == "Pratama":  # kondisi 3
+    print("Ternyata Pratama")
+else:
+    print(f"Siapa namamu tadi? {nama} ya?")
