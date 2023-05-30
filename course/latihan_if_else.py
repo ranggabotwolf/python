f599e58138c9bf6db2a85f57143f5b81e20f958b
ranggabotwolf <kecul37.com>
2023-05-30 10:52:44 +0700

    if else statement

--- course/latihan_if_else.py
+++ course/latihan_if_else.py
@@ -8,5 +8,20 @@
 nama = input("Nama anda siapa? ")
 
 # * 1. Program if inline
+# if nama == "Rangga": print(f"Kamu Pintar {nama}")
+# print(f"Terima kasih {nama}")
+# code diatas menggunakan komentar karena tidak bisa menggunakan if inline akibat install extension yang otomatis merapikan source code
+
+# * 2. Program if indentation
 if nama == "Rangga":
     print(f"Kamu Pintar {nama}")
+    print(f"Teruslah belajar {nama}")
+# 2 baris code diatas masih berada dalam if, sedangkan code dibawah sudah diluar if
+print(f"Terimakasih banyak {nama}")
+
+# * Else Statement
+if nama == "Rangga":
+    print(f"Kamu berhasil {nama}")
+else:
+    print(f"Aaaaahh kamu bukan {nama}")
+print(f"Thanks {nama}")
