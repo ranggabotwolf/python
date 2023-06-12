a78d3cf601215df313b13d9aef9f80f4bed93e51
ranggabotwolf <kecul37@gmail.com>
2023-06-12 19:20:03 +0700

    continue & pass

---
+++ course/latihan_continuepass.py
@@ -0,0 +1,34 @@
+# Control flow terdiri dari:
+# Continue, pass, break
+
+# pass -> dia berfungsi sebagai dummy, tidak akan dieksekusi
+
+angka = 0
+while angka < 5:
+    angka += 1
+    if angka == 2:
+        print("ini angka ke 2")
+    if angka == 4:
+        pass  # baris ini tidak akan dieksekusi / dijalankan
+    print(angka)
+
+# diatas merupakan contoh penggunaan pass
+# pass biasanya digunakan untuk metode / class yang tidak digunakan tapi tetap dideklarasikan
+# maka dari itu tujuan digunakannya pass
+
+
+# continue
+
+number = 0
+while number < 6:
+    number += 1
+    print(f"now is number -> {number}")  # aksi 1
+    if number == 3:
+        print("Keren")
+    if number == 5:
+        print("Mantap")
+        continue
+    print(f"nomor {number} berhasil di cek")  # aksi 2
+    # apapun yang berada dibawah continue akan diskip bila sudah sesuai kondisi code di atas continue nya
+    # seperti pada contoh di atas, aksi 2 di skip setelah number = 5
+print("Selesai")
